75dc7c6ba4ff182c50c3f7b63614921428450b5c
CoraPei <cora.junbo@ericsson.com>
2019-08-01 16:22:17 +0800

    1st commit

---
+++ jump7.py
@@ -0,0 +1,10 @@
+for i in range(1,101):
+    if i==17 or i==27 or i==37 or i==47 or i==57 or i==67 or i==87 or i==97:
+        pass
+    elif i >=71 and i<=79:
+        pass
+    elif i%7 == 0:
+        pass
+    else:
+        print(i)
+
